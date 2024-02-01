1310e0bcca16573f33bf7e938f56c88dc033de7f
Fazlı Altun <32793348+fazlialtunn@users.noreply.github.com>
2024-02-02 01:03:02 +0300

    Profile Page is created. API configs stuff

---
+++ Spotify-Clone/Models/SettingsModels.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  Spotify-Clone
+//
+//  Created by Fazli Altun on 1.02.2024.
+//
+
+import Foundation
---
+++ Spotify-Clone/Resources/StringLocalization.swift
@@ -0,0 +1,8 @@
+//
+//  StringLocalization.swift
+//  Spotify-Clone
+//
+//  Created by Fazli Altun on 1.02.2024.
+//
+
+import Foundation
